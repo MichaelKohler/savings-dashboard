4921dd28421549e03385994c4407c3ebb1b85794
Michael Kohler <me@michaelkohler.info>
2025-02-02 22:45:53 +0100

    Redirect root to accounts view

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
 import { requireUserId } from "~/session.server";
 
 export function meta(): ReturnType<MetaFunction> {
@@ -12,9 +12,5 @@ export function meta(): ReturnType<MetaFunction> {
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await requireUserId(request);
-  return json({});
-}
-
-export default function Index() {
-  return <p>Select a category from the left to get started!</p>;
+  return redirect("/accounts");
 }
