c3bf0d27fca14c4dce66a157d5eb47226b696190
Michael Kohler <me@michaelkohler.info>
2025-02-02 22:36:49 +0100

    Move nav to the bottom

--- app/components/main.tsx
+++ app/components/main.tsx
@@ -5,104 +5,80 @@ export default function Sidebar() {
   const user = useOptionalUser();
 
   return (
-    <div className="flex bg-white">
-      <div
-        className={`${
-          !user && "hidden"
-        } min-h-screen w-10 border-r md:block md:w-40`}
-      >
-        <ul>
-          {!user && (
-            <li>
-              <NavLink
-                className={({ isActive }) =>
-                  `align-items text-l flex w-full flex-row gap-1 border-b p-4 ${
-                    isActive ? "bg-white" : ""
-                  }`
-                }
-                to="/login"
-              >
-                Login
-              </NavLink>
-            </li>
-          )}
-          {user && (
-            <>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    `align-items text-l flex w-full flex-row justify-center gap-1 border-b py-4 md:justify-start md:p-4 ${
-                      isActive ? "bg-white" : ""
-                    }`
-                  }
-                  to="/accounts"
-                >
-                  🏦<span className="ml-2 hidden md:inline">Accounts</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    `align-items text-l flex w-full flex-row justify-center gap-1 border-b py-4 md:justify-start md:p-4 ${
-                      isActive ? "bg-white" : ""
-                    }`
-                  }
-                  to="/groups"
-                >
-                  🫙<span className="ml-2 hidden md:inline">Groups</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    `align-items text-l flex w-full flex-row justify-center gap-1 border-b py-4 md:justify-start md:p-4 ${
-                      isActive ? "bg-white" : ""
-                    }`
-                  }
-                  to="/types"
-                >
-                  🏦<span className="ml-2 hidden md:inline">Types</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    `align-items text-l flex w-full flex-row justify-center gap-1 border-b py-4 md:justify-start md:p-4 ${
-                      isActive ? "bg-white" : ""
-                    }`
-                  }
-                  to="/balances"
-                >
-                  💰<span className="ml-2 hidden md:inline">Balances</span>
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  className={({ isActive }) =>
-                    `align-items text-l flex w-full flex-row justify-center gap-1 border-b py-4 md:justify-start md:p-4 ${
-                      isActive ? "bg-white" : ""
-                    }`
-                  }
-                  to="/charts"
-                >
-                  📈<span className="ml-2 hidden md:inline">Charts</span>
-                </NavLink>
-              </li>
-              <li className="align-items text-l flex w-full flex-row justify-center gap-1 border-b py-4 md:justify-start md:p-4">
-                <Form action="/logout" method="post">
-                  <button type="submit">
-                    🔓 <span className="ml-2 hidden md:inline">Logout</span>
-                  </button>
-                </Form>
-              </li>
-            </>
-          )}
-        </ul>
-      </div>
-
-      <div className="flex-1 p-6">
+    <div className="bg-white flex flex-col h-screen">
+      <div className="p-4 md:p-8 grow">
         <Outlet />
       </div>
+      <ul className="sticky bottom-0 bg-white w-screen flex flex-row justify-between py-4 px-6 md:px-12 border-t shadow-[rgba(0,0,0,0.1)_0px_-3px_50px_5px]">
+        {!user && (
+          <li>
+            <NavLink
+              className="flex flex-col justify-center items-center py-4"
+              to="/login"
+            >
+              <span>🔐</span>
+              <span className="mt-2 hidden md:inline">Login</span>
+            </NavLink>
+          </li>
+        )}
+        {user && (
+          <>
+            <li>
+              <NavLink
+                className="flex flex-col justify-center items-center py-4"
+                to="/accounts"
+              >
+                <span>🏦</span>
+                <span className="mt-2 hidden md:inline">Accounts</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="flex flex-col justify-center items-center py-4"
+                to="/groups"
+              >
+                <span>🫙</span>
+                <span className="mt-2 hidden md:inline">Groups</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="flex flex-col justify-center items-center py-4"
+                to="/types"
+              >
+                <span>🏦</span>
+                <span className="mt-2 hidden md:inline">Types</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="flex flex-col justify-center items-center py-4"
+                to="/balances"
+              >
+                <span>💰</span>
+                <span className="mt-2 hidden md:inline">Balances</span>
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                className="flex flex-col justify-center items-center py-4"
+                to="/charts"
+              >
+                <span>📈</span>
+                <span className="mt-2 hidden md:inline">Charts</span>
+              </NavLink>
+            </li>
+            <li className="flex flex-col justify-center items-center py-4">
+              <Form action="/logout" method="post">
+                <button type="submit" className="flex flex-col">
+                  <span>🔓</span>
+                  <span className="mt-2 hidden md:inline">Logout</span>
+                </button>
+              </Form>
+            </li>
+          </>
+        )}
+      </ul>
     </div>
   );
 }
