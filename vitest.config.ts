75ebb9722bb07cef7fdbee75d711a7353ed6766e
Michael Kohler <me@michaelkohler.info>
2025-07-23 21:14:35 +0200

    Exclude E2E tests for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults } from "vitest/config";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     environment: "happy-dom",
+    exclude: [...configDefaults.exclude, "e2e/"],
   },
 });
