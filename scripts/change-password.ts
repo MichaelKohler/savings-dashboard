7c5c60412c7b89dc5519234721164da3c49baa0c
Michael Kohler <me@michaelkohler.info>
2025-08-03 17:44:19 +0200

    Simplify

--- scripts/change-password.ts
+++ scripts/change-password.ts
@@ -1,11 +1,14 @@
 import * as readline from "readline/promises";
 import { stdin as input, stdout as output } from "process";
-import { changePassword } from "../app/models/user.server";
-import { prisma } from "../app/db.server";
+
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "@node-rs/bcrypt";
 
 const rl = readline.createInterface({ input, output });
 
 async function main() {
+  const prisma = new PrismaClient();
+
   const email = await rl.question(
     "Enter the email of the user to change the password for: "
   );
@@ -16,7 +19,26 @@ async function main() {
     return;
   }
 
-  await changePassword(email, password);
+  const existingUser = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (!existingUser) {
+    throw new Error("USER_NOT_FOUND");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await prisma.password.update({
+    where: {
+      userId: existingUser.id,
+    },
+    data: {
+      hash: hashedPassword,
+    },
+  });
 
   console.log(`Password for user ${email} changed successfully. 🌱`);
 }
@@ -27,6 +49,5 @@ main()
     process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect();
     rl.close();
   });
--- scripts/create-user.ts
+++ scripts/create-user.ts
@@ -1,11 +1,14 @@
 import * as readline from "readline/promises";
 import { stdin as input, stdout as output } from "process";
-import { createUser } from "../app/models/user.server";
-import { prisma } from "../app/db.server";
+
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "@node-rs/bcrypt";
 
 const rl = readline.createInterface({ input, output });
 
 async function main() {
+  const prisma = new PrismaClient();
+
   const email = await rl.question("Enter the email for the new user: ");
   const password = await rl.question("Enter the password for the new user: ");
 
@@ -14,7 +17,18 @@ async function main() {
     return;
   }
 
-  await createUser(email, password);
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await prisma.user.create({
+    data: {
+      email,
+      password: {
+        create: {
+          hash: hashedPassword,
+        },
+      },
+    },
+  });
 
   console.log(`User ${email} created successfully. 🌱`);
 }
@@ -25,6 +39,5 @@ main()
     process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect();
     rl.close();
   });
